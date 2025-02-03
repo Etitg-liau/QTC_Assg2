52e011475db6bc97b2ff7ff1cc4efa0ac2b95c20
Etitg-liau <petr.liauos@gmail.com>
2025-02-03 23:54:48 +0800

    Major Addition of testing qwalks layouts

--- walks.py
+++ walks.py
@@ -6,6 +6,7 @@ from qiskit.visualization import plot_histogram
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import community as coummnity_louvain
 
 #100 qubits for testing
 
@@ -49,15 +50,62 @@ def create_network_graph():
             G.add_edge(i, i +1)
         else:
             i += 1
+    botnet_nodes = range(50, 100) 
+    for node in botnet_nodes:
+        for other_node in botnet_nodes:
+            if node != other_node:
+                G.add_edge(node, other_node)
+    return G
     
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='red', node_size=2000)
-    plt.title("Network Graph Representing Connections")
-    plt.show()
+    #pos = nx.kamada_kawai_layout(G)
+    #nx.draw(G, pos, with_labels=True, node_color='green', edge_color='red', node_size=2000, font_size=7)
+    #plt.title("Botnet Detection Algorithm?")
+    #plt.show()
 
+def draw_graph_with_layouts(G):
+    # Set up the figure
+    fig, axes = plt.subplots(2, 2, figsize=(15, 15))
+    
+    # Spring Layout
+    pos_spring = nx.spring_layout(G, k=0.1, iterations=50)
+    axes[0, 0].set_title("Spring Layout")
+    nx.draw(G, pos_spring, with_labels=True, node_color='lightblue', edge_color='gray', node_size=2000, font_size=8, ax=axes[0, 0])
+
+    # Kamada-Kawai Layout
+    pos_kamada = nx.kamada_kawai_layout(G)
+    axes[0, 1].set_title("Kamada-Kawai Layout")
+    nx.draw(G, pos_kamada, with_labels=True, node_color='lightgreen', edge_color='gray', node_size=2000, font_size=8, ax=axes[0, 1])
+
+    # Circular Layout
+    pos_circular = nx.circular_layout(G)
+    axes[1, 0].set_title("Circular Layout")
+    nx.draw(G, pos_circular, with_labels=True, node_color='lightcoral', edge_color='gray', node_size=2000, font_size=8, ax=axes[1, 0])
+
+    # Spectral Layout
+    pos_spectral = nx.spectral_layout(G)
+    axes[1, 1].set_title("Spectral Layout")
+    nx.draw(G, pos_spectral, with_labels=True, node_color='lightyellow', edge_color='gray', node_size=2000, font_size=8, ax=axes[1, 1])
+
+    # Adjust layout
+    plt.tight_layout()
+    plt.show()    
+
+def detect_communities(G):
+    # Apply Louvain community detection algorithm
+    partition = community_louvain.best_partition(G)
+    
+    # Draw the graph with communities
+    plt.figure(figsize=(10, 10))
+    pos = nx.kamada_kawai_layout(G)
+    cmap = plt.get_cmap('Set3')
+    nx.draw(G, pos, node_color=[cmap(partition[node]) for node in G.nodes()], with_labels=True, node_size=2000, font_size=8)
+    plt.title("Superposition of Qubits")
+    plt.show()
+    
 def main():
     num_qubits = 100
-    create_network_graph()
+    G = create_network_graph()
+    draw_graph_with_layouts(G)
     
     qc = create_quantum_walk_circuit(num_qubits)
     counts = simulate_quantum_walk(qc)
